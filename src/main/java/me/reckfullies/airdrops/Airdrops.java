2c50e2d2522313ab5bba2f5754ddd8a87fc6d1e8
Devon Gowen <reckfullies@gmail.com>
2019-06-12 02:13:31 -0400

    Initial Commit

---
+++ src/main/java/me/reckfullies/airdrops/Airdrops.java
@@ -0,0 +1,18 @@
+package me.reckfullies.airdrops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Airdrops extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
